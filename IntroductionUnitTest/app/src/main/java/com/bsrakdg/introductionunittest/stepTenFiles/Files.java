37e15413427c48960adebc1be7bfcb63a71a329e
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-02-15 01:02:30 +0300

    AssertJ File exception thrown added

---
+++ IntroductionUnitTest/app/src/main/java/com/bsrakdg/introductionunittest/stepTenFiles/Files.java
@@ -0,0 +1,4 @@
+package com.bsrakdg.introductionunittest.stepTenFiles;
+
+public class Files {
+}
---
+++ IntroductionUnitTest/app/src/test/java/com/bsrakdg/introductionunittest/stepTenFiles/FilesTest.java
@@ -0,0 +1,44 @@
+package com.bsrakdg.introductionunittest.stepTenFiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.contentOf;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import com.bsrakdg.introductionunittest.common.MailServerUnavailableException;
+
+import org.junit.Test;
+import org.junit.runners.model.MultipleFailureException;
+
+import java.io.File;
+
+public class FilesTest {
+
+    @Test
+    public void testFile() throws Exception {
+        File file = new File("/Users/bserdaroglu/GithubWorks/LearningUnitTestJava/IntroductionUnitTest/app/src/test/resources/test.txt");
+        // new File("{copyFilePathOn/app/src/test/resources/test.txt");
+        assertThat(file)
+                .exists()
+                .canRead()
+                .canWrite()
+                .hasName("test.txt");
+    }
+
+    @Test
+    public void testContentOfContentFile() throws Exception {
+        File file = new File("/Users/bserdaroglu/GithubWorks/LearningUnitTestJava/IntroductionUnitTest/app/src/test/resources/test.txt");
+        // new File("{copyFilePathOn/app/src/test/resources/test.txt");
+        assertThat(contentOf(file))
+                .startsWith("Hi");
+    }
+
+    @Test
+    public void testFileException() {
+        Exception exception = new MailServerUnavailableException("Error message");
+
+        assertThat(exception)
+                .hasMessage("Error message")
+                .isInstanceOf(MailServerUnavailableException.class)
+                .hasMessageContaining("message");
+
+    }
+}
