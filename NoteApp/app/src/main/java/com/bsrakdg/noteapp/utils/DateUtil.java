6da17583fd612e11dca55fd184f4ed377cc595b6
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-06-19 21:00:44 +0300

    DateUtil and test class create for showing on recyclerview item

---
+++ NoteApp/app/src/main/java/com/bsrakdg/noteapp/utils/DateUtil.java
@@ -0,0 +1,74 @@
+package com.bsrakdg.noteapp.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+    // Database : 06-2020
+    // UI : June 2020 
+
+    public static final String[] monthNumbers = {"01", "02", "03", "04", "05", "06", "07", "08",
+            "09", "10", "11", "12"};
+    private static final String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug"
+            , "Sep", "Oct", "Nov", "Dec"};
+    private static final String GET_MONTH_ERROR = "Error. Invalid month number.";
+    private static final String DATE_FORMAT = "MM-yyyy";
+    private static final String TAG = "Utility";
+
+    public static String getCurrentTimeStamp() throws Exception {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT); //MUST USE LOWERCASE 'y'. API 23- can't use uppercase
+            return dateFormat.format(new Date()); // Find today's date
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception("Couldn't format the date into MM-yyyy");
+        }
+    }
+
+    public static String getMonthFromNumber(String monthNumber) {
+        switch (monthNumber) {
+            case "01": {
+                return months[0];
+            }
+            case "02": {
+                return months[1];
+            }
+            case "03": {
+                return months[2];
+            }
+            case "04": {
+                return months[3];
+            }
+            case "05": {
+                return months[4];
+            }
+            case "06": {
+                return months[5];
+            }
+            case "07": {
+                return months[6];
+            }
+            case "08": {
+                return months[7];
+            }
+            case "09": {
+                return months[8];
+            }
+            case "10": {
+                return months[9];
+            }
+            case "11": {
+                return months[10];
+            }
+            case "12": {
+                return months[11];
+            }
+
+            default: {
+                return GET_MONTH_ERROR;
+            }
+        }
+    }
+
+
+}
---
+++ NoteApp/app/src/test/java/com/bsrakdg/noteapp/utils/DateUtilTest.java
@@ -0,0 +1,7 @@
+package com.bsrakdg.noteapp.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateUtilTest {
+
+}
