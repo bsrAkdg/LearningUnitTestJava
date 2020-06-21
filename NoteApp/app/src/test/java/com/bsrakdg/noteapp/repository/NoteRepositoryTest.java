78ef81e403aa48a0caf619a9cf7078c2ee369755
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-06-21 17:37:38 +0300

    Unit testing data insertion from repository (Success, Fail, Exception)

--- NoteApp/app/src/test/java/com/bsrakdg/noteapp/repository/NoteRepositoryTest.java
+++ NoteApp/app/src/test/java/com/bsrakdg/noteapp/repository/NoteRepositoryTest.java
@@ -1,16 +1,31 @@
 package com.bsrakdg.noteapp.repository;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static com.bsrakdg.noteapp.repository.NoteRepository.INSERT_FAILURE;
+import static com.bsrakdg.noteapp.repository.NoteRepository.INSERT_SUCCESS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import com.bsrakdg.noteapp.models.Note;
 import com.bsrakdg.noteapp.persistence.NoteDao;
+import com.bsrakdg.noteapp.ui.Resource;
+import com.bsrakdg.noteapp.utils.TestUtil;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import io.reactivex.Single;
+
 // @TestInstance(TestInstance.Lifecycle.PER_CLASS) // if you use BeforeAll, you need to add this
 class NoteRepositoryTest {
+
+    private static final Note NOTE1 = new Note(TestUtil.TEST_NOTE_1);
 
     // system under test
     private NoteRepository noteRepository;
@@ -23,7 +38,7 @@ class NoteRepositoryTest {
      * so it can reset the conditions for the next one.
      */
     @BeforeEach // individual test
-    public void initEach() {
+    void initEach() {
         MockitoAnnotations.initMocks(this);
         noteRepository = new NoteRepository(noteDao);
     }
@@ -37,9 +52,79 @@ class NoteRepositoryTest {
         MockitoAnnotations.initMocks(this);
         noteRepository = new NoteRepository(noteDao);
     }*/
+
+    /*
+        Insert note
+        verify the correct method is called
+        confirm observer is triggered
+        confirm new rows inserted
+    */
     @Test
-    void dummyTest() throws Exception {
-        assertNotNull(noteDao);
-        assertNotNull(noteRepository);
+    void insertNote_returnRow() throws Exception {
+        // Arrange
+        final Long insertedRow = 1L;
+        final Single<Long> returnedData = Single.just(insertedRow); // NoteDao -> insertNote
+
+        when(noteDao.insertNote(any(Note.class))).thenReturn(returnedData);
+
+        // Act
+        final Resource<Integer> returnedValue = noteRepository.insertNote(NOTE1)
+                .blockingFirst(); // NoteRepository -> insertNote
+
+        // Assert
+        verify(noteDao).insertNote(any(Note.class));
+        verifyNoMoreInteractions(noteDao);
+
+        System.out.println("Returned values : " + returnedValue.data);
+        assertEquals(Resource.success(1, INSERT_SUCCESS), returnedValue);
+
+        // Or test using RxJava
+        /*
+        noteRepository.insertNote(NOTE1)
+                .test()
+                .await() // like blockingFirst
+                .assertValue(Resource.success(1, INSERT_SUCCESS));
+        */
     }
+
+    /*
+        Insert note
+        Failure (return -1)
+    */
+    @Test
+    void insertNote_returnFailure() throws Exception {
+        // Arrange
+        final Long failedInsertedRow = -1L;
+        final Single<Long> returnedData = Single.just(failedInsertedRow); // NoteDao -> insertNote
+
+        when(noteDao.insertNote(any(Note.class))).thenReturn(returnedData);
+
+        // Act
+        final Resource<Integer> returnedValue = noteRepository.insertNote(NOTE1)
+                .blockingFirst(); // NoteRepository -> insertNote
+
+        // Assert
+        verify(noteDao).insertNote(any(Note.class));
+        verifyNoMoreInteractions(noteDao);
+
+        assertEquals(Resource.error(null, INSERT_FAILURE), returnedValue);
+    }
+
+    /*
+        Insert note
+        null title
+        confirm throw exception
+    */
+    @Test
+    void insertNote_nullTitle_throwException() throws Exception {
+        assertThrows(Exception.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                final Note note = new Note(TestUtil.TEST_NOTE_1);
+                note.setTitle(null);
+                noteRepository.insertNote(note);
+            }
+        });
+    }
+
 }
